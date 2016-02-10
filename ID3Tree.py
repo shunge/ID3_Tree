f493b54ca46072d344770aa7ac5c409294a21cd1
shunge <gyishun@gmail.com>
2016-02-10 10:20:18 -0800

    Entropy
    edited

--- ID3Tree.py
+++ ID3Tree.py
@@ -1,5 +1,10 @@
 import math
 
+trainingData = []
+attributesData = []
+
+
+
 class Node:
 	def __init__(self, parent=None):
 		self.parent = parent
@@ -16,7 +21,7 @@ def getMajority(data):
 			count[0] += 1
 		elif(d[4] == 2):
 			count[1] += 1
-		else
+		else:
 			count[2] += 1
 
 	return max(count)
@@ -71,11 +76,71 @@ def buildID3Tree(data, attributes, root):
 def ID3Tree(data, attributes):
 	return (data, attributes, Node())
 
+
+
+def loadData():
+    f = open('hw3train.txt', 'r')
+    trainingData = [map(float, line.split()) for line in f]
+    listx1 = []
+    listx2 = []
+    listx3 = []
+    listx4 = []
+    length = len(listx1)
+    for data in trainingData:
+        listx1.append(data[0])
+        listx2.append(data[1])
+        listx3.append(data[2])
+        listx4.append(data[3])
+    for i in range(length-1):
+        trainingData.append([1, ((listx1[i]+listx1[i+1])/2)])
+        trainingData.append([2, ((listx2[i]+listx2[i+1])/2)])
+        trainingData.append([3, ((listx3[i]+listx3[i+1])/2)])
+        trainingData.append([4, ((listx4[i]+listx4[i+1])/2)])
+
+
+
+def entropy(a, b, c):
+    sum = float(a+b+c)
+    a = float(a/sum)
+    b = float(b/sum)
+    c = float(c/sum)
+    entro = - (a) * math.log(a, 2) \
+            - (b) * math.log(b, 2) - (c) * math.log(c, 2)
+    return entro
+
+def totalentropy(rawdata, attr):
+    intlist = [0] * 6
+    sum = 0
+    for data in rawdata:
+        label = data[:-1]
+        if data[attr[0]] > attr[1]:
+            if label == 1:
+                intlist[0] +=1
+            elif label == 2:
+                intlist[1] +=1
+            else:
+                intlist[2] +=1
+        else:
+            if label == 1:
+                intlist[3] +=1
+            elif label == 2:
+                intlist[4] +=1
+            else:
+                intlist[5] +=1
+    for int in intlist:
+        sum+=int
+    ProbGre =  float((intlist[0]+intlist[1]+intlist[2])/sum)
+    ProbLeq =  float((intlist[3]+intlist[4]+intlist[5])/sum)
+    return ProbGre*entropy(intlist[0],intlist[1],intlist[2]) \
+            + ProbLeq*entropy(intlist[3],intlist[4],intlist[5])
+
 def printTree(root):
+    return
+
 
 def main():
-	root = ID3Tree()
-	printTree()
+    root = ID3Tree()
+    printTree()
 
 
 
